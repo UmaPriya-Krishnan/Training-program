bb115a8b464fa869118c64cb17610b4b8acab703
UmaPriya-Krishnan <65336672+UmaPriya-Krishnan@users.noreply.github.com>
2020-06-22 13:07:47 +0530

    Update ut_fns_02.py

--- Python/grp_session/ut_fns_02.py
+++ Python/grp_session/ut_fns_02.py
@@ -1,4 +1,5 @@
 def btd(str1,str2):
+    ''' To concat the given two strings and convert it from binary to decimal'''
     num = str1+str2
     num = int(num)
     rem, binn, power, i = 0,0,0,0
@@ -13,6 +14,7 @@ str1 = '01'
 str2 = '10'
 
 def isPal(string):
+    '''To check whether the given string is a palindrome or not'''
     for i in range(0, len(string)//2):
         if string[i] != string[len(string)-i-1]:
             return False
@@ -21,6 +23,7 @@ def isPal(string):
 string = 'malayalam'
     
 def date(year):
+    '''To get the difference between the entered year and given year(1970) in years, days, hours, minutes and seconds'''
     yr = abs(int(year) - 1970)
     if (yr%4 == 0 and yr%100 !=0) or yr%400 == 0 :
         days = yr*366
