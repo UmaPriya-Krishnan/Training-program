0d5ac38c9c63025aebec357c3c1bdf711563579a
UmaPriya-Krishnan <65336672+UmaPriya-Krishnan@users.noreply.github.com>
2020-06-24 16:21:41 +0530

    Update ut_04.py

--- Python/grp_session/ut_04.py
+++ Python/grp_session/ut_04.py
@@ -11,7 +11,14 @@ class MyTest(unittest.TestCase):
         string = 'nf3124fsf9823'
         ph_no = ut_fns_04.number(string)
         self.assertEqual(ph_no, '31249823fs')
-        
+    def test_MatrixSwap(self):
+        listt = [[2,7,1,8],
+                 [1,9,5,0],
+                 [3,6,7,8]]
+        swapped = ut_fns_04.swap(listt)
+        self.assertEqual(swapped, [[2,7,1,8],
+                                   [1,7,6,0],
+                                   [3,9,5,8]])        
 if __name__ == '__main__':
     unittest.main()
     
