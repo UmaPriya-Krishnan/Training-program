7573f2fc7d33e0b74c9e61b7faa1dde6990e3724
UmaPriya-Krishnan <65336672+UmaPriya-Krishnan@users.noreply.github.com>
2020-11-23 11:29:54 +0530

    Update dbconnect.go

--- cred/dbconnect.go
+++ cred/dbconnect.go
@@ -6,6 +6,8 @@ import (
 
 	_ "github.com/lib/pq"
 )
+github.com/umapriya-krishnan/Training-program v0.0.0-20201119122526-f8d989c4d61f h1:fDxzN9gZznZXKXwVXjLGP2jZRToRCmEWbgw6zVQFOEA=
+github.com/umapriya-krishnan/Training-program v0.0.0-20201119122526-f8d989c4d61f/go.mod h1:9bUq9cPRq8gOzAeo0AY/iDpaPYlqP7exCHIHzZzMQck=
 
 const (
 	host     = "localhost"
